2a544945bd1139984b8875fd7869fd73523dcab1
David Richfield <davidrichfield@gmail.com>
2016-04-16 13:09:22 +0200

    remove admin dependencies

--- goclubproj/settings.py
+++ goclubproj/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- goclubproj/urls.py
+++ goclubproj/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 import goclubdb.views
 
 urlpatterns = [
     url(r'^$', goclubdb.views.index, name='index'),
     url(r'^db', goclubdb.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
