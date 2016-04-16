dd7c7cf9a9c94de6a85f7f4860a199f3fd348ab1
David Richfield <davidrichfield@gmail.com>
2016-04-16 12:52:53 +0200

    Renamed app to goclubdb

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import goclubdb.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', goclubdb.views.index, name='index'),
+    url(r'^db', goclubdb.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
