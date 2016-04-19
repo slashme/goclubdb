401756902e32cac16eed9781a3ecdbd9559ea027
David Richfield <davidrichfield@gmail.com>
2016-04-19 08:02:51 +0200

    Add delete links and templates for clubs

--- goclubproj/urls.py
+++ goclubproj/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^layer/(?P<pk>[0-9]+)/$', goclubdb.views.LayerDetail.as_view(), name='layerdetail'),
     url(r'^layer/(?P<pk>[0-9]+)/edit', goclubdb.views.LayerUpdate.as_view(), name='update_layer'),
     url(r'^club/(?P<pk>[0-9]+)/edit', goclubdb.views.ClubUpdate.as_view(), name='update_club'),
+    url(r'^club/(?P<pk>[0-9]+)/delete', goclubdb.views.ClubDelete.as_view(success_url="/"), name='delete_club'),
     url(r'^club/create', goclubdb.views.ClubCreate.as_view(), name='create_club'),
     url(r'^layer/create', goclubdb.views.LayerCreate.as_view(), name='create_layer'),
     url(r'^club/(?P<name>[A-Z]+)/create', goclubdb.views.ClubCreateLayer.as_view(), name='create_club'),
