28ef30b9ca97144d5dbd2b888800fc5c46c7719a
David Richfield <davidrichfield@gmail.com>
2016-04-24 11:40:39 +0200

    Comment out reddit, not working yet

--- goclubproj/settings.py
+++ goclubproj/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.google',
     #'allauth.socialaccount.providers.linkedin_oauth2',
-    'allauth.socialaccount.providers.reddit',
+    #'allauth.socialaccount.providers.reddit',
     'allauth.socialaccount.providers.vk',
     'avatar',
 )
