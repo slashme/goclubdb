1d0e4e0f4e86b1b4173cf76b0ac88b72434554f4
David Richfield <davidrichfield@gmail.com>
2019-02-17 20:30:20 +0100

    Force HTTPS

--- goclubproj/settings.py
+++ goclubproj/settings.py
@@ -132,6 +132,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
