dc7387ca663b96f4ddec1e8f913faaa9af009d93
David Richfield <davidrichfield@gmail.com>
2021-10-25 19:30:09 +0200

    Remove secret
    I've created a new secret for the production version. This version will
    not contain it, for security reasons.

--- goclubproj/settings.py
+++ goclubproj/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
